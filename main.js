a68a5492b57ed1e828eb9fd0809926fbfc974e74
arush-hue <77105133+arush-hue@users.noreply.github.com>
2021-09-07 17:34:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modelLoaded);
+}
+function draw()
+{
+image(video,0,0,300,300);
+classifier.classify(video, gotResult);
+}
+function modelLoaded()
+{
+console.log("Model Loaded!");
+}
+function gotResult(error, result)
+{
+if(error)
+{
+    console.error(error);
+}else{
+    console.log(result);
+    document.getElementById("m").innerHTML=result[0].label;
+    document.getElementById("h").innerHTML=result[0].confidence.toFixed(3);
+}
+}
+
+
